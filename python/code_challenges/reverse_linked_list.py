7c2e0a97dc8ea8736cf310580ac5bbc33178f9ec
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-10-10 23:29:33 +0300

    code challenge 1

--- python/code_challenges/reverse_linked_list.py
+++ python/code_challenges/reverse_linked_list.py
@@ -1,9 +1,11 @@
-def reverse_list(ll):
-    """Reverses a linked list
-    Args:
-        ll: linked list
-    Returns:
-        linked list in reversed form
-    """
-    # put your function implementation here
-    return ll
+list=[1,2,3,4,5,6,-99,100]
+def reverseArray(list):
+ size=len(list)
+ m=int(size/2)
+ print(list)
+ for i in range(m):
+    change=list[i]
+    list[i]=list[size-i-1]
+    list[size-i-1]=change
+    return list
+print (reverseArray(list))
