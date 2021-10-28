34a778de726d579317beb9430857d169b16fd829
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-10-28 19:52:39 +0300

    4 tests passes

--- python/trees/tests/test_tress_file.py
+++ python/trees/tests/test_tress_file.py
@@ -50,30 +50,30 @@ from trees.tress import BinaryTree, Node
 #   assert actual == expected
 #   print("test_bfs_2 passed")
 
-# def test_pre_order():
-#   # Arrange
-#   # Create tree instance
-#   tree = BinaryTree()
+def test_pre_order():
+  # Arrange
+  # Create tree instance
+  tree = BinaryTree()
 
-#   # Create Nodes for 1,2,3,4
-#   a_node = Node('1')
-#   b_node = Node('2')
-#   c_node = Node('3')
-#   d_node = Node('4')
-#   a_node.left = b_node
-#   a_node.right = c_node
-#   b_node.left = d_node
+  # Create Nodes for 1,2,3,4
+  a_node = Node('1')
+  b_node = Node('2')
+  c_node = Node('3')
+  d_node = Node('4')
+  a_node.left = b_node
+  a_node.right = c_node
+  b_node.left = d_node
 
-#   # Add Root node to tree
-#   tree.root=a_node
+  # Add Root node to tree
+  tree.root=a_node
 
-#   # set expected list
-#   expected = ["1", "2", "4", "3"]
-#   # set actual to return value of pre_order call
-#   actual = tree.pre_order()
-#   # assert actual is same as expected
-#   assert actual == expected
-#   print("test_pre_order_ passed")
+  # set expected list
+  expected = ["1", "2", "4", "3"]
+  # set actual to return value of pre_order call
+  actual = tree.pre_order()
+  # assert actual is same as expected
+  assert actual == expected
+  print("test_pre_order_ passed")
 
 # def test_post_order():
 #   # Arrange
@@ -153,3 +153,17 @@ def test_add_lr_node():
     expected='3'
     actual=tree.root.right.data
     assert expected == actual
+'''Can successfully return a collection from a preorder traversal'''
+def test_pre_order():
+  tree = BinaryTree()
+  a_node = Node('1')
+  b_node = Node('2')
+  c_node = Node('3')
+  d_node = Node('4')
+  a_node.left = b_node
+  a_node.right = c_node
+  b_node.left = d_node
+  tree.root=a_node
+  expected = ["1", "2", "4", "3"]
+  actual = tree.pre_order()
+  assert actual == expected
