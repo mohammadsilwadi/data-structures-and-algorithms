bf19d007ec1b64c1456c4b74e23c59a85503e141
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-18 19:40:40 +0200

    test:  tests passed

--- python/hash/tests/test_hash_table.py
+++ python/hash/tests/test_hash_table.py
@@ -11,6 +11,13 @@ def test_hash_add(hashtable):
     actual= hashtable.get('march 8')
     assert actual == expected
 
+"""Successfully returns null for a key that does not exist in the hashtable"""
+def test_hash_not_exist(hashtable):
+    hashtable.add('march 8' , 400)
+    expected=None
+    actual= hashtable.get('march 10')
+    assert actual == expected
+
 
 def test_hash(hashtable):
 	expected = 700
