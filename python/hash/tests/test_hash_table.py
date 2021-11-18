07ba910954310ff401d971f3631f4e63648b57dd
mohammad silwadi <mohammadsilwadi1@gmail.com>
2021-11-18 19:38:38 +0200

    test: 4 tests passed

---
+++ python/hash/tests/test_hash_table.py
@@ -0,0 +1,23 @@
+from hash_tables.hash_table import HashTable
+import pytest
+
+@pytest.fixture
+def hashtable():
+	return HashTable()
+'''Adding a key/value to your hashtable results in the value being in the data structure'''
+def test_hash_add(hashtable):
+    hashtable.add('march 8' , 400)
+    expected=400
+    actual= hashtable.get('march 8')
+    assert actual == expected
+
+
+def test_hash(hashtable):
+	expected = 700
+	actual = hashtable._HashTable__hash("d")
+	assert actual == expected
+
+def test_hash_word(hashtable):
+	expected = 376
+	actual =  hashtable._HashTable__hash("dd")
+	assert actual == expected
